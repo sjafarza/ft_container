0deff898fa239cbb450e5583c4e633d93093ecef
user42 <sjafarza@student.42.fr>
2022-07-16 16:16:29 +0200

    finsh test of const vector

--- test_vector.cpp
+++ test_vector.cpp
@@ -9,8 +9,8 @@
 
 # define NORMAL "\033[0;37m"
 # define YELLOW "\033[0;33m"
-# define RED "\033[1;31m"
-# define GREEN "\033[032m"
+# define RED    "\033[1;31m"
+# define GREEN  "\033[032m"
 
 template <class T>
 std::string vector_is_equal(const std::vector<T> & std_vector, const ft::vector<T> & ft_vector)
@@ -99,6 +99,7 @@ void    test_constructor()
     {
         std::cerr << "Error : faild to open file" << std::endl;
     }
+    /* Detault constructor */
     std::vector<int>     std_v1;
     ft::vector<int>      ft_v1;
     out << printVectorAttributes(std_v1,ft_v1, "Test default constructor");
@@ -106,24 +107,48 @@ void    test_constructor()
     std::cout << "*****************************Test constructor***********************************************." << std::endl;
     std::cout << " test default constructor \t\t\t\t\t\t\t\t"<< vector_is_equal(std_v1 , ft_v1) << std::endl;
 
-    std::vector<int>    std_v2(5, 100);
-    ft::vector<int>     ft_v2(5, 100);
-    out << printVectorAttributes(std_v2,ft_v2, "Test fill constructor");
-    std::cout << NORMAL " test fill   constructor \t\t\t\t\t\t\t\t"<< vector_is_equal(std_v2 , ft_v2) << std::endl;
+    /* Detault pointer constructor */
+    std::vector<int>*    std_pointer_default_v =  new std::vector<int>;
+    ft::vector<int>*    ft_pointer_default_v =  new ft::vector<int>;
+    out << printVectorAttributes( *std_pointer_default_v,*ft_pointer_default_v, "Test default pointer constructor");
+    std::cout << NORMAL " test default pointer constructor \t\t\t\t\t\t\t"<< vector_is_equal(*std_pointer_default_v, *ft_pointer_default_v) << std::endl;
 
+    /* Fill constructor */
+    std::vector<int>    std_v2(5);
+    ft::vector<int>     ft_v2(5);
+    out << printVectorAttributes(std_v2,ft_v2, "Test fill constructor");
+
+    /*Fill constructor size*/
+    std::vector<int>    std_v3(5, 100);
+    ft::vector<int>     ft_v3(5, 100);
+    out << printVectorAttributes(std_v3,ft_v3, "Test fill constructor size");
+    std::cout << NORMAL " test fill constructor  size\t\t\t\t\t\t\t\t"<< vector_is_equal(std_v3 , ft_v3) << std::endl;
+
+    /*Range constructor */
     std::vector<int>    s_test(5, 7);
     ft::vector<int>     ft_test(5, 7);
-    std::vector<int>    std_v3(s_test.begin() + 1, s_test.end() - 2);
-    ft::vector<int>     ft_v3(ft_test.begin() + 1, ft_test.end() - 2);
-     out << printVectorAttributes(std_v3,ft_v3, "Test range constructor");
-    std::cout << NORMAL " test range  constructor \t\t\t\t\t\t\t\t"<< vector_is_equal(std_v3 , ft_v3) << std::endl;
+    std::vector<int>    std_v4(s_test.begin() + 1, s_test.end() - 2);
+    ft::vector<int>     ft_v4(ft_test.begin() + 1, ft_test.end() - 2);
+    out << printVectorAttributes(std_v4,ft_v4, "Test range of another vector constructor");
+    std::cout << NORMAL " test range of another vector constructor \t\t\t\t\t\t"<< vector_is_equal(std_v4 , ft_v4) << std::endl;
+    
+    int range_array[] = { 15, 65397429, 1299, 965, 5 , 88, 9};
+    std::vector<int>::iterator std_itr(&(range_array[1]));
+    ft::vector<int>::iterator ft_itr(&(range_array[1]));
 
-    std::vector<int>    std_v4(s_test);
-    ft::vector<int>     ft_v4(ft_test);
-     out << printVectorAttributes(std_v4,ft_v4, "Test copy constructor");
-    std::cout << NORMAL " test copy  constructor \t\t\t\t\t\t\t\t"<< vector_is_equal(std_v4 , ft_v4) << std::endl;
+    std::vector<int>    std_arr_v4(std_itr , std_itr + 3);
+    ft::vector<int>     ft_arr_v4(ft_itr , ft_itr + 3);
+    out << printVectorAttributes(std_arr_v4,ft_arr_v4, "Test range of array constructor");
+    std::cout << NORMAL " test range of array constructor \t\t\t\t\t\t\t"<< vector_is_equal(std_arr_v4 , ft_arr_v4) << std::endl;
 
+    /*Copy constructor */
+    std::vector<int>    std_v5(s_test);
+    ft::vector<int>     ft_v5(ft_test);
+     out << printVectorAttributes(std_v5,ft_v5, "Test copy constructor");
+    std::cout << NORMAL " test copy  constructor \t\t\t\t\t\t\t\t"<< vector_is_equal(std_v5 , ft_v5) << std::endl;
 
+    delete  ft_pointer_default_v;
+    delete  std_pointer_default_v;
     out.close();
 }
 
